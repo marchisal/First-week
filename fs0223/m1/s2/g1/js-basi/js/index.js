7ad18afc49669f929937bae08b101ff3684d540c
marchisal <salismariachiara@gmail.com>
2023-04-03 16:47:59 +0200

    Esercizi inventati su JS

---
+++ fs0223/m1/s2/g1/js-basi/js/index.js
@@ -0,0 +1,88 @@
+var a = 90
+var b = 100
+console.log (a + b)
+
+var b = 80
+console.log (a - b)
+
+
+
+
+var cane = 'Jack Russel'
+var a = cane
+console.log(a)
+
+var gatto = 'Certosino'
+var b = gatto
+console.log(b)
+
+if(b == a){
+    console.log('Litigio')
+}else{
+    console.log('ok')
+}
+
+
+var a=6
+var b=56
+var c='Chicco'
+console.log(c + ' ha ' + (b + a) + ' anni')
+
+
+/*COMMENTATO PER EVITARE CONTINUO ALERT
+
+var dog=prompt('Hai un cane o un gatto?')
+if (dog=='cane'){
+    alert('croccantino!')
+}else{
+    alert('pappa morbida!')
+}*/
+//-------------------------------------------------------------------
+
+/*COMMENTATO PER EVITARE CONTINUO ALERT
+
+document.getElementById("pet").innerHTML="Hai un cane o un gatto?";
+
+function sceltaCane(){
+    alert('Hai scelto il cane!')
+}
+
+function sceltaGatto(){
+    alert('Hai scelto il gatto!')
+}*/
+//------------------------------------------------------------------------
+
+/*COMMENTATO PER EVITARE CONTINUO ALERT
+
+var n = prompt("Quanti anni hai?")
+if (n!=4 && n>=5 && n<=10){
+    console.log("Visione adatta ai bambini tra 5 e 10 anni")
+
+}else if(n>=11 && n<=17){
+    console.log("Visione adatta ai ragazzi")
+}else if(n>120){
+    console.log("Non dire cazzate!")
+}else{
+    console.log("Visione adatta ad un pubblico adulto!")
+}*/
+
+
+//---------------------------------------------------------------------
+
+
+
+/*COMMENTATO PER EVITARE CONTINUO ALERT
+var nome = prompt("Inserisci il tuo nome")
+var lastName = prompt("Inserisci il tuo cognome")
+console.log(nome + ' ' + lastName)*/
+
+
+//---------------------------------------------------------------------
+
+
+
+
+
+
+
+
